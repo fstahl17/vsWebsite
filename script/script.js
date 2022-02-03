7a311c3a72ff31c730e4f6a69fc45d52a0d8b41c
Felix17 <fstahl@student.tgm.ac.at>
2022-02-03 19:44:35 +0100

    commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+const logo = document.querySelectorAll('#logo path');
+console.log(logo);
+
+for(let i = 0; i<logo.length; i++){
+  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
+
+function copyToClipboard(text) {
+  const elem = document.createElement('textarea');
+  elem.value = text;
+  document.body.appendChild(elem);
+  elem.select();
+  document.execCommand('copy');
+  document.body.removeChild(elem);
+}
